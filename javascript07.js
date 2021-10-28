dea69f866269fb1e13a914507decb0750f06a10f
Ivanilson DEV 21 <89939411+IvanilsonDaCosta21@users.noreply.github.com>
2021-10-28 15:43:44 -0100

    hoje 28-10-21

--- javascript07.js
+++ javascript07.js
@@ -1,12 +1,12 @@
-function somar(){
+//function somar(){
     var tn1 = window.document.getElementById('txtn1')
     var tn2 = window.document.querySelector('input#txtn2')
     var res = window.document.getElementById('res')
-    // var n1 = Number(tn1.value)
+    var n1 = Number(tn1.value)
     var n2 = Number(tn2.value)
-    var s = n1 + n2 
-    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
-}
+//    var s = n1 + n2 
+//    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
+//}
 
 
  function mudarConteudo(){
@@ -28,43 +28,57 @@ function somar(){
 
 
 
-
+//Usando Operadores Matemáticos
 
  var operadores = window.document.getElementById('operadores')
  var soma = window.document.getElementById('soma');
  var multiplicacao = window.document.getElementById('multiplicacao')
  var subtracao = window.document.querySelector('#subtracao')
  var divisao = window.document.querySelector('#divisao')
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
  function operando(){
   
-    switch (operadores) {
+    switch (operadores){
         case soma:
-            res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
-            res.value = n1 + n2
+            var soma = window.document.getElementById('soma')
+                var s = n1 + n2
+                  res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
+                         
             break;
         case multiplicacao:
+            var multiplicacao = window.document.getElementById('multiplicacao') 
+            s = n1 * n2
             res.innerHTML = `A Multiplicacao entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
-            res.value = n1 * n2
             break;
+            
         case subtracao:
-            res.innerHTML = `A Subtracao entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
-            res.value = n1 - n2
             
+           
+            s = n1 - n2
+             res.innerHTML = `A Subtracao entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
             break;
-        case divisao:
-            res.innerHTML = `A Subtracao entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
-            res.value = n1 / n2
             
+            
+        case divisao:
+           
+            s = n1 / n2
+             res.innerHTML = `A Subtracao entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
             break;
     
         default: 'Escolha um dos operadores'
             break;
-    }
+        }
 
 
 
- }
+        }
 
 
 
